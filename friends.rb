d7ecde62cebb15f3e3c2136ad984b9faf14b73c2
Young Koh <youngheon6@gmail.com>
2023-01-04 13:36:47 -0700

    add jimmy if statement

--- friends.rb
+++ friends.rb
@@ -3,11 +3,15 @@ bestfriend = "Johnnie"
 birthdate = "12/10"
 bestfriend_2 = "Vinnie"
 birthdate_2 = "6/10"
+bestfriend_3 = "Jimmy"
+birthdate_3 = "3/10"
 
 if birthdate == "12/10" && bestfriend == "Johnnie"
     puts "Happy birthday Johnnie!"
 elsif birthdate_2 == "6/10" && bestfriend_2 == "Vinnie"
     puts "Happy birthday Vinnie!"
+elsif birthdate_3 == "3/10" && bestfriend_3 == "Jimmy"
+    puts "Happy birthday Jimmy"
 else
     puts "It's nobody's birthday"
 end
