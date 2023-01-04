64d28f5854b15396f774358a639aa4c5b8be5ffd
Young Koh <youngheon6@gmail.com>
2023-01-04 13:31:05 -0700

    add if statement

--- friends.rb
+++ friends.rb
@@ -1,1 +1,13 @@
 #Pairing Excercise 2
+bestfriend = "Johnnie"
+birthdate = "12/10"
+bestfriend_2 = "Vinnie"
+birthdate_2 = "6/10"
+
+if birthdate == "12/10" && bestfriend == "Johnnie"
+    puts "Happy birthday Johnnie!"
+elsif birthdate_2 == "6/10" && bestfriend_2 == "Vinnie"
+    puts "Happy birthday Vinnie!"
+else
+    puts "It's nobody's birthday"
+end
